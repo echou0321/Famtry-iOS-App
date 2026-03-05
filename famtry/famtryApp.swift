1ff7735a94f295240a994c1ba889e56f82bc60ab
aaronli16 <liaaron53@gmail.com>
2026-03-04 20:15:15 -0800

    Initial Commit

---
+++ famtry/famtryApp.swift
@@ -0,0 +1,17 @@
+//
+//  famtryApp.swift
+//  famtry
+//
+//  Created by Aaron Li on 3/4/26.
+//
+
+import SwiftUI
+
+@main
+struct famtryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ famtryTests/famtryTests.swift
@@ -0,0 +1,17 @@
+//
+//  famtryTests.swift
+//  famtryTests
+//
+//  Created by Aaron Li on 3/4/26.
+//
+
+import Testing
+@testable import famtry
+
+struct famtryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
